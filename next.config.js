a36fc241a80fa8a23a1de1705192f473edfeb940
RoyKim-WB <102020443+RoyKim-WB@users.noreply.github.com>
2022-04-08 17:22:18 +0900

    🍀 Implement: Github Pages 연동

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  assetPrefix:
+    process.env.NODE_ENV === "production" ? "https://devxe.github.io/blog" : "",
+  images: {
+    loader: "akamai",
+    path: "",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
