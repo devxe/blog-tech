9935e6052c0b13e15f449324b69ad1be26b78577
RoyKim-WB <102020443+RoyKim-WB@users.noreply.github.com>
2022-04-11 18:43:01 +0900

    🎨 Design: Post Detail 화면 퍼블리싱

--- components/PostDetail.tsx
+++ components/PostDetail.tsx
@@ -1,9 +1,20 @@
 import { NextPage } from "next";
 import { useRouter } from "next/router";
 import React from "react";
+import { useSelector } from "react-redux";
+import { monthDayYear } from "../lib/utils";
+import { RootState } from "../store";
 
 const PostDetail: NextPage = () => {
   const router = useRouter();
+  const { id } = router.query;
+  const postId = Number(id);
+  const { data } = useSelector((state: RootState) => state.user);
+  const post = data?.filter((post) => post.id === postId)[0];
+  const { title, date } = post || {
+    title: "",
+    date: "",
+  };
 
   const handleBack = () => {
     router.back();
@@ -11,7 +22,25 @@ const PostDetail: NextPage = () => {
 
   return (
     <>
-      <button className="flex felx-row items-center" onClick={handleBack}>
+      <span className="text-2xl font-bold">{title}</span>
+      <div className="text-xs flex flex-row items-center space-x-1 text-gray-400 mt-4">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-4 w-4"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          strokeWidth={2}
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
+          />
+        </svg>
+        <span>{monthDayYear(date)}</span>
+      </div>
+      {/* <button className="flex felx-row items-center" onClick={handleBack}>
         <svg
           xmlns="http://www.w3.org/2000/svg"
           className="h-4 w-4"
@@ -27,9 +56,7 @@ const PostDetail: NextPage = () => {
           />
         </svg>
         <span className="text-sm">목록으로 가기</span>
-      </button>
-
-      <span></span>
+      </button> */}
     </>
   );
 };
--- pages/post/[id].tsx
+++ pages/post/[id].tsx
@@ -1,21 +1,10 @@
 import { NextPage } from "next";
-import { useRouter } from "next/router";
-import { useSelector } from "react-redux";
 import PostDetail from "../../components/PostDetail";
-import { RootState } from "../../store";
 
 const PostDetailPage: NextPage = () => {
-  const router = useRouter();
-  const { id } = router.query;
-
-  const postId = Number(id);
-  const { data } = useSelector((state: RootState) => state.user);
-  const post = data?.filter((post) => post.id === postId)[0];
-
   return (
     <>
       <PostDetail />
-      <h1>Post Detail {id}</h1>
     </>
   );
 };
